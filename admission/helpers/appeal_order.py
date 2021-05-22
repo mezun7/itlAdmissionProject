7ea4878b41bc6d5dc6d212d790c89202ba216eea
Shuhrat Azimuratov <mezun7@users.noreply.github.com>
2021-05-22 09:32:27 +0300

    Major changes

--- admission/helpers/appeal_order.py
+++ admission/helpers/appeal_order.py
@@ -2,7 +2,7 @@ from first_tour.models import UserAppeal
 
 
 def get_appeal_order(participant):
-    uas = UserAppeal.objects.all().order_by('id')
+    uas = UserAppeal.objects.all().order_by('appeal_apply_time')
     for ind, ua in enumerate(uas):
         if ua.participant == participant:
             return ind
